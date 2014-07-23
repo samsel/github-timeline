a31b1198b54e43255fd676f3463e4f46e9f4f729
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-07-22 19:48:28 -0700

    added metainfo about each bookmark node

---
+++ src/js/node.js
@@ -0,0 +1,16 @@
+/*
+	BookmarkTreeNode
+	https://developer.chrome.com/extensions/bookmarks#type-BookmarkTreeNode
+
+	_definition of each bookmark node object_
+
+	{
+		id                 : <string>,
+		parentId(optional) : <string>,
+		index(optional)    : <integer>,
+		url (optional)     : <string>,
+		title              : <string>,
+		children           : <array of nodes>	
+	}
+
+*/
